3d8c0c29857521aecfce9c00cbe70d2e161c106e
jn <j.nerche@kontext-e.de>
2017-05-09 22:40:52 +0200

    Initial commit

---
+++ src/main/java/de/kontext_e/blueprints/jqa_gradle/Main.java
@@ -0,0 +1,7 @@
+package de.kontext_e.blueprints.jqa_gradle;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/de/kontext_e/blueprints/jqa_gradle/MainTest.java
@@ -0,0 +1,11 @@
+package de.kontext_e.blueprints.jqa_gradle;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void someTest() throws Exception {
+    }
+
+}
